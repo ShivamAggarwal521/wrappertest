7ef1117c9d77c6de7f97b72893188decc482b2ea
Shivam Aggarwal <aggarwalshivam002@gmail.com>
2025-09-20 23:29:48 +0530

    make proper web UI

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,47 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+// vite.config.ts
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: "dist",
+//     lib: {
+//       // just give the relative path directly
+//       entry: "src/widget.tsx",
+//       name: "ChatbotWidget",
+//       fileName: () => "chatbot-widget.js",
+//       formats: ["iife"],
+//     },
+//   },
+// });
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist",
+    lib: {
+      // just give the relative path directly
+      entry: "src/widget.tsx",
+      name: "ChatbotWidget",
+      fileName: () => "chatbot-widget.js",
+      formats: ["iife"],
+    },
+  },
+  server: {
+    allowedHosts: [
+      '.ngrok-free.app', // allow all ngrok subdomains
+      'localhost'         // keep localhost allowed
+    ]
+  }
+});
+  
