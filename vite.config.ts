a02a255f09feaa4bffc8de9a2a22f12b84885958
Shivam Aggarwal <aggarwalshivam002@gmail.com>
2025-10-23 16:15:02 +0530

    vdtcfhgdvg

--- vite.config.ts
+++ vite.config.ts
@@ -27,6 +27,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "./", // Important for Vercel deployment
   build: {
     outDir: "dist",
     emptyOutDir: true
